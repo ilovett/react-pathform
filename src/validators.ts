3e18d4501181c1df2a0cb60dea365d615d864593
Ian Lovett <i.c.lovett@gmail.com>
2021-03-11 16:00:33 -0800

    update `validateYupSchema` to not abort early, throw on validation error

--- src/validators.ts
+++ src/validators.ts
@@ -2,12 +2,16 @@ import { fromDotPath } from './usePathForm';
 
 export const validateYupSchema = (schema: any) => async (values: any, addError: any) => {
   try {
-    await schema?.validate(values);
+    await schema?.validate(values, { abortEarly: false });
   } catch (err) {
+    // inner is available when `abortEarly: false`
     err?.inner?.forEach?.((validationError: any) => {
       const path = fromDotPath(validationError.path);
       const { type, message, value } = validationError;
       addError(path, { type, message, value });
     });
+
+    // bubble up yup schema error, stops submission
+    throw err;
   }
 };
