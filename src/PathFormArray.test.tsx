e3d905f50532563b1fe9ed6d31185a1c61d6b164
Ian Lovett <i.c.lovett@gmail.com>
2021-03-28 09:33:24 -0700

    added test for `PathFormArray`

---
+++ src/PathFormArray.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { getAllByLabelText, render } from '@testing-library/react';
+import { PathFormProvider } from './usePathForm';
+import { PathFormArray } from './PathFormArray';
+import { PathFormField } from './PathFormField';
+
+const TestWrapper: React.FC = ({ children }) => {
+  return <PathFormProvider initialRenderValues={{ items: [{ name: 'Person A' }, { name: 'Person B' }] }}>{children}</PathFormProvider>;
+};
+
+describe('PathFormArray', () => {
+  let container: HTMLElement;
+  let debug: any;
+
+  beforeEach(() => {
+    ({ container, debug } = render(
+      <PathFormArray
+        path={['items']}
+        defaultValue={[]}
+        render={(arrayProps, meta) => {
+          return (
+            <PathFormField
+              path={[...arrayProps.itemPath, 'name']}
+              defaultValue=""
+              render={(inputProps, meta, renders) => (
+                <div>
+                  <label htmlFor={`name-${meta.uuid}`}>Name</label>
+                  <input id={`name-${meta.uuid}`} {...inputProps} />
+                  <pre data-testid="meta">{JSON.stringify(meta)}</pre>
+                  <pre data-testid="renders">{JSON.stringify(renders)}</pre>
+                  <pre data-testid="inputProps">{JSON.stringify(inputProps)}</pre>
+                </div>
+              )}
+            />
+          );
+        }}
+        renderEmpty={() => <>Empty!</>}
+      />,
+      { wrapper: TestWrapper }
+    ));
+  });
+
+  it('renders the values from the store', async () => {
+    expect(getAllByLabelText(container, 'Name')[0]).toHaveDisplayValue('Person A');
+    expect(getAllByLabelText(container, 'Name')[1]).toHaveDisplayValue('Person B');
+  });
+});
